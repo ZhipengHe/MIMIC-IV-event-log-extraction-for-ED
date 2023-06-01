5a9009d6f7173adc1935744292440e06fb9239c1
Zhipeng He <me.zhipeng.he@gmail.com>
2023-06-01 16:30:54 +1000

    Removed 59 cases with invalid ED length of stay

---
+++ 1_extract_eventlog/4_clean.sql
@@ -0,0 +1,19 @@
+/*
+Add 4_clean.sql
+1. remove cases of which intime >= outtime - validity
+*/
+
+/*
+1. Remove cases of which intime >= outtime
+*/
+
+DROP TABLE IF EXISTS "mimic_insights"."ed_tables_clean_rule";
+SELECT * 
+INTO "mimic_insights"."ed_tables_clean_rule"
+FROM "mimic_insights"."ed_tables"
+WHERE stay_id IN (
+    SELECT stay_id
+    FROM "mimiciv_ed"."edstays"
+    WHERE intime < outtime
+);
+
