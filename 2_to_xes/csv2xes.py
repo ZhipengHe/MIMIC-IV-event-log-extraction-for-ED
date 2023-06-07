3b660a557ea2c88d993c2bc373e1a975827d7fd4
Zhipeng He <me.zhipeng.he@gmail.com>
2023-06-07 14:23:21 +1000

    Fix issue #4

--- 2_to_xes/csv2xes.py
+++ 2_to_xes/csv2xes.py
@@ -11,50 +11,37 @@ import pm4py
 import numpy as np
 import pandas as pd
 from pm4py.objects.log.util import dataframe_utils
-from pm4py.objects.conversion.log import converter as log_converter
-from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 
 # %% [markdown]
 # ### Import CSV log into pandas dataframe
 # Please change the path for your CSV file below.
 
 # %%
-csv_file_path = "mimicel.csv.gz" # or "your_file_path_here.csv"
+csv_file_path = "mimicel.csv" # or "your_file_path_here.csv"
 
-# log_csv = pd.read_csv(csv_file_path, sep=',')
-log_csv = pd.read_csv(csv_file_path, sep=',', compression='gzip', header=0)
-
-# %%
-log_csv.head(20)
+log_csv = pd.read_csv(csv_file_path, sep=',', dtype=object) 
+# log_csv = pd.read_csv(csv_file_path, sep=',', compression='gzip', header=0)
 
 # %% [markdown]
 # ### Process the dataframe and convert it to `pm4py` event log structure
 # The default attributes in `pm4py` package:
 # 
-# - Case ID --> case:concept:name
-# - Activity --> concept:name
-# - Timestamps --> time:timestamp
 # - Case attributes -->  start with `case:`
 
 # %%
 # rename some attributes name
 log_csv.rename(columns=
     {
-        # Standardization for CaseID, activity and timestamp
-        'stay_id':'case:concept:name',
-        'activity':'concept:name',
-        'timestamps':'time:timestamp', 
-
-        # Standardization for Case attributes
+        # case attributes
+        'stay_id': 'case:concept:name',
         'subject_id': 'case:subject_id', 
         'hadm_id':'case:hadm_id', 
         'acuity': 'case:acuity', 
         'chiefcomplaint': 'case:chiefcomplaint',
-        
-        # new case attributes
-        'gender': 'case:gender',
-        'race': 'case:race',
-    }, inplace=True)
+        # standard activity and timestamp name
+        'activity': 'concept:name',
+        'timestamps': 'time:timestamp'
+        }, inplace=True)
 
 # %% [markdown]
 # `pm4py` will select values in the first row of each case for case attributes. Thus, we need fill in rows with empty case attribute
@@ -64,8 +51,6 @@ log_csv.rename(columns=
 # %%
 log_csv['case:acuity'] = log_csv.groupby('case:concept:name')['case:acuity'].transform(lambda v: v.ffill().bfill())
 log_csv['case:chiefcomplaint'] = log_csv.groupby('case:concept:name')['case:chiefcomplaint'].transform(lambda v: v.ffill().bfill())
-log_csv['case:gender'] = log_csv.groupby('case:concept:name')['case:gender'].transform(lambda v: v.ffill().bfill())
-log_csv['case:race'] = log_csv.groupby('case:concept:name')['case:race'].transform(lambda v: v.ffill().bfill())
 
 # %% [markdown]
 # `pm4py` has built-in fuctions for transforming the data type of timestamp in the dataframe.
@@ -76,11 +61,6 @@ log_csv['case:race'] = log_csv.groupby('case:concept:name')['case:race'].transfo
 
 # %%
 log_csv = dataframe_utils.convert_timestamp_columns_in_df(log_csv)
-log_csv = log_csv.sort_values('time:timestamp')
-
-# %%
-# check the first 20 rows
-log_csv.head(20)
 
 # %% [markdown]
 # ### Export event log data to XES file
@@ -88,27 +68,13 @@ log_csv.head(20)
 # The default export setting for exporting XES file requires using the default column name of case id `case:concept:name`. But you can use parameters to specify a different name.
 
 # %%
-dataframe = log_csv.head(200000)
-
-event_log_selected = log_converter.apply(dataframe, variant=log_converter.Variants.TO_EVENT_LOG)
-
-# %%
-xes_file_path = "mimicel-test.xes"
-xes_exporter.apply(event_log_selected, xes_file_path, parameters={xes_exporter.Variants.ETREE.value.Parameters.COMPRESS: True})
+event_log = pm4py.convert_to_event_log(log_csv, stream_postprocessing=True)
+xes_file_path = "mimicel.xes"
+pm4py.write_xes(event_log, xes_file_path, case_id_key='case:concept:name')
 
 # %%
-# # You can set parameters for using different column name of case id
-# parameters = {log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'case:stay_id'}
-# event_log = log_converter.apply(log_csv, parameters=parameters, variant=log_converter.Variants.TO_EVENT_LOG)
-
-# default usage
-event_log = log_converter.apply(log_csv, variant=log_converter.Variants.TO_EVENT_LOG)
-
-# %%
-xes_file_path = "mimicel.xes" # or "your_file_path_here.xes"
-xes_exporter.apply(event_log, xes_file_path, parameters={xes_exporter.Variants.ETREE.value.Parameters.COMPRESS: True})
-
-# %%
-
-
+# examples
+example_csv = log_csv.head(100000)
+example_event_log = pm4py.convert_to_event_log(example_csv, stream_postprocessing=True) 
+pm4py.write_xes(example_event_log, 'mimicel_example.xes', case_id_key='case:concept:name')
 
